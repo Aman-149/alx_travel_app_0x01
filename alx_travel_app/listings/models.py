5b050a36b4a750d3e50fcbf93fb79e2641305748
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-30 22:02:28 +0300

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,1 +1,19 @@
+from django.db import models
+
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bookings')
+    guest_name = models.CharField(max_length=100)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f"{self.guest_name} - {self.listing.name}"
 
